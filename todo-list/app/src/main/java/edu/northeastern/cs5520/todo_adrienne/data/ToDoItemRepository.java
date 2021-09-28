d6dceff02071822ab6d79d22eccb753bd39c5fbc
Adrienne Slaughter <ahslaughter@northeastern.edu>
2021-09-28 13:05:43 -0700

    Cleaning repo implementation and beginning to add change listeners

--- todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/data/ToDoItemRepository.java
+++ todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/data/ToDoItemRepository.java
@@ -1,20 +1,55 @@
 package edu.northeastern.cs5520.todo_adrienne.data;
 
-import java.util.HashSet;
-import java.util.Set;
+import androidx.annotation.NonNull;
 
-public class ToDoItemRepository {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class ToDoItemRepository implements Iterable<ToDo>{
 
     private static Set<ToDo> todoSet;
 
+    private static ToDoItemRepository singleton;
+
     private ToDoItemRepository() {
         todoSet = new HashSet<ToDo>();
+        this.createFakeData();
     }
 
-    public static Set<ToDo> getAllTodos() {
-        if (todoSet == null) {
-            todoSet = new HashSet<ToDo>();
+    public static ToDoItemRepository getAllTodos() {
+        if (singleton == null) {
+            singleton = new ToDoItemRepository();
         }
-        return todoSet;
+        return singleton;
+    }
+
+    public static void addToDo(ToDo newToDo) {
+        getAllTodos().todoSet.add(newToDo);
+    }
+
+    private void createFakeData() {
+        todoSet.add(ToDo.createTodo("Task todo 1", "do something, already"));
+        todoSet.add(ToDo.createTodo("Task todo 2", "and another thign!"));
+
+    }
+
+    @NonNull
+    @Override
+    public Iterator<ToDo> iterator() {
+        return todoSet.iterator();
+    }
+
+    @Override
+    public void forEach(@NonNull Consumer<? super ToDo> action) {
+        todoSet.forEach(action);
+    }
+
+    @NonNull
+    @Override
+    public Spliterator<ToDo> spliterator() {
+        return todoSet.spliterator();
     }
 }
---
+++ todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/data/ToDoItemRepositoryChangeListener.java
@@ -0,0 +1,7 @@
+package edu.northeastern.cs5520.todo_adrienne.data;
+
+public interface ToDoItemRepositoryChangeListener {
+
+    public void ToDoItemAdded(ToDo newToDoItem);
+
+}
