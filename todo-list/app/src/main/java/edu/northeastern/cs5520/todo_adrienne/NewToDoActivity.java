de04e5d0427405335d942bfbd21db2dff6cbe434
Adrienne Slaughter <ahslaughter@northeastern.edu>
2021-09-28 13:04:16 -0700

    Creating new Activity to create new Todos

---
+++ todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/NewToDoActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.cs5520.todo_adrienne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_to_do);
+    }
+}
