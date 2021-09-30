9d33e84a68ce7fb226c4d6b6510032318cd56fd3
Adrienne Slaughter <ahslaughter@northeastern.edu>
2021-09-30 15:31:09 -0700

    Creating a new Activity that holds a ListView using a ViewModel

---
+++ todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/MainActivity_ListView.java
@@ -0,0 +1,14 @@
+package edu.northeastern.cs5520.todo_adrienne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list_view);
+    }
+}
