989a3bb7d9fd90bd74ac84b871cb6336293a2c3f
Adrienne Slaughter <ahslaughter@northeastern.edu>
2021-09-30 19:22:50 -0700

    Deleting dead code.

--- todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/ToDoFragment.java
+++ todo-list/app/src/main/java/edu/northeastern/cs5520/todo_adrienne/ToDoFragment.java
@@ -1,25 +1,19 @@
 package edu.northeastern.cs5520.todo_adrienne;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import edu.northeastern.cs5520.todo_adrienne.data.ToDo;
-import edu.northeastern.cs5520.todo_adrienne.data.ToDoItemRepository;
 import edu.northeastern.cs5520.todo_adrienne.databinding.FragmentToDoBinding;
-import edu.northeastern.cs5520.todo_adrienne.databinding.FragmentToDoListBinding;
+
 
 public class ToDoFragment extends Fragment {
 
@@ -42,6 +36,7 @@ public class ToDoFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 
+        // Wire up the button to
         binding.buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,19 +44,7 @@ public class ToDoFragment extends Fragment {
             }
         });
 
-//        items.setOnClickListener(item -> {
-//            // Set a new item
-//            viewModel.select(item);
-//        });
     }
-
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        mViewModel = new ViewModelProvider(this).get(ToDoViewModel.class);
-//        // TODO: Use the ViewModel
-//
-//    }
 
 
 }
